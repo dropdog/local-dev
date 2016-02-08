0f0ccd1dd643eeb7135dbaf1e88aff1614a562ae
TheodorosPloumis <me@theodorosploumis.com>
2016-02-08 17:42:32 +0200

    Clear development values not suitable for live

--- settings.local.php
+++ settings.local.php
@@ -15,73 +15,13 @@
  */
 
 /**
- * Assertions.
- *
- * The Drupal project primarily uses runtime assertions to enforce the
- * expectations of the API by failing when incorrect calls are made by code
- * under development.
- *
- * @see http://php.net/assert
- * @see https://www.drupal.org/node/2492225
- *
- * If you are using PHP 7.0 it is strongly recommended that you set
- * zend.assertions=1 in the PHP.ini file (It cannot be changed from .htaccess
- * or runtime) on development machines and to 0 in production.
- *
- * @see https://wiki.php.net/rfc/expectations
+ * Enable CSS and JS aggregation.
  */
-assert_options(ASSERT_ACTIVE, TRUE);
-\Drupal\Component\Assertion\Handle::register();
+$config['system.performance']['css']['preprocess'] = TRUE;
+$config['system.performance']['js']['preprocess'] = TRUE;
 
 /**
- * Show all error messages, with backtrace information.
- *
- * In case the error level could not be fetched from the database, as for
- * example the database connection failed, we rely only on this value.
- */
-$config['system.logging']['error_level'] = 'verbose';
-
-/**
- * Disable CSS and JS aggregation.
- */
-$config['system.performance']['css']['preprocess'] = FALSE;
-$config['system.performance']['js']['preprocess'] = FALSE;
-
-/**
- * Disable the render cache (this includes the page cache).
- *
- * Note: you should test with the render cache enabled, to ensure the correct
- * cacheability metadata is present. However, in the early stages of
- * development, you may want to disable it.
- *
- * This setting disables the render cache by using the Null cache back-end
- * defined by the development.services.yml file above.
- *
- * Do not use this setting until after the site is installed.
- */
-$settings['cache']['bins']['render'] = 'cache.backend.null';
-$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
-
-/**
- * Disable Dynamic Page Cache.
- *
- * Note: you should test with Dynamic Page Cache enabled, to ensure the correct
- * cacheability metadata is present (and hence the expected behavior). However,
- * in the early stages of development, you may want to disable it.
- */
-# $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
-
-/**
- * Allow test modules and themes to be installed.
- *
- * Drupal ignores test modules and themes by default for performance reasons.
- * During development it can be useful to install test extensions for debugging
- * purposes.
- */
-#$settings['extension_discovery_scan_tests'] = TRUE;
-
-/**
- * Enable access to rebuild.php.
+ * Disable access to rebuild.php.
  *
  * This setting can be enabled to allow Drupal's php and database cached
  * storage to be cleared via the rebuild.php page. Access to this page can also
@@ -97,11 +37,5 @@ $settings['update_free_access'] = FALSE;
  * Enable local development services.
  */
 $settings['container_yamls'][] = DRUPAL_ROOT . '/local-dev/live.services.yml';
-
-// Config sync folder
-$config_directories = array(
-	CONFIG_SYNC_DIRECTORY => DRUPAL_ROOT . '/config_customdev/sync',
-	CONFIG_STAGING_DIRECTORY => DRUPAL_ROOT . '/config_customdev/staging/',
-);
 
 
